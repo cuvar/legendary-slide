bccd900fc1e6465076acfb600fde167e2f95d5cc
cuvar <luca-mueller@gmx.net>
2022-10-04 15:02:54 +0200

    feat: basic twind setup

--- src/components/slide.ts
+++ src/components/slide.ts
@@ -3,24 +3,22 @@ import { customElement, property } from "lit/decorators.js";
 import { theme } from "../styles/theme";
 import "../styles/index.css";
 
+import { create, cssomSheet } from "twind";
+const sheet = cssomSheet({ target: new CSSStyleSheet() });
+const { tw } = create({ sheet });
+
 @customElement("legendary-slide")
 export class SlideComponent extends LitElement {
-  static styles = [
-    theme,
-    css`
-      :host {
-        display: block;
-        color: var(--color-primary);
-      }
-    `,
-  ];
+  static styles = [sheet.target];
 
   @property() name = "Legendary Slide";
 
   render() {
     return html`
       <div>
-        <h1 class="bg-green-500">Hello, ${this.name} this is luca</h1>
+        <h1 class=${tw`text-green-500 font-sans`}>
+          Hello, ${this.name} this is luca
+        </h1>
       </div>
     `;
   }
